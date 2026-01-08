d214a2ad5147f0d563d75650a9245ff987fc0f6f
jesse <jesse.robert.perkins@gmail.com>
2026-01-07 19:21:45 -0800

    Added functionality to the nav bar and added LinkedIn box.

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+}
